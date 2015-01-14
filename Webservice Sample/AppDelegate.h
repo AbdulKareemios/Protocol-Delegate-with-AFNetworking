0e226d613c222c0643435001d5d8b6c8be75dba1
Abdul Kareem <AbdulKareem@abdul.local>
2015-01-14 13:39:49 +0400

    Initial Commit

---
+++ Webservice Sample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Webservice Sample
+//
+//  Created by Abdul Kareem on 1/14/15.
+//  Copyright (c) 2015 MapletMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Webservice Sample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Webservice Sample
+//
+//  Created by Abdul Kareem on 1/14/15.
+//  Copyright (c) 2015 MapletMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
